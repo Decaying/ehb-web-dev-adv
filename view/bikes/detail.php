854642dc5eadb0967eaa6d093fcf17ed3d2b2860
Hans Buys <buyshans@gmail.com>
2017-05-15 21:18:27 +0200

    refactored bike detail

--- view/bikes/detail.php
+++ view/bikes/detail.php
@@ -16,29 +16,37 @@ class Detail implements View {
     }
 
     public function render() {
-        echo '
-        <h2>' . $this->model->bike->name . '</h2>
-        <p> Category: ' . $this->model->bike->category . '</p>
-        <p>' . $this->model->bike->description . '</p>
-<div class="row">
-    <div class="col-sm-6 col-md-6 col-lg-6">
+        $bike = $this->model->bike;
+        $name = $bike->name;
+        $category = $bike->name;
+        $description = $bike->description;
+        $image = $bike->image;
+        $price = $bike->getPriceWithCurrency();
+        $id = $bike->id;
+
+        echo "
+<h2>$name</h2>
+<p>Category: $category</p>
+<p>$description</p>
+<div class='row'>
+    <div class='col-sm-6 col-md-6 col-lg-6'>
         
-        <p><img src="' . $this->model->bike->image . '" class="img-responsive img-thumbnail"></p>
+        <p><img src='$image' class='img-responsive img-thumbnail'></p>
         
     </div>
-    <div class="col-sm-6 col-md-6 col-lg-6">
-        <p>Price : ' . $this->model->bike->getPriceWithCurrency() . '</p>
-        <p><a href="#" data-id="' . $this->model->bike->id . '" class="btn btn-success shop-item-button">
-            <span class="glyphicon glyphicon-shopping-cart"></span>
+    <div class='col-sm-6 col-md-6 col-lg-6'>
+        <p>Price : $price</p>
+        <p><a href='#' data-id='$id' class='btn btn-success shop-item-button'>
+            <span class='glyphicon glyphicon-shopping-cart'></span>
             Add to cart
         </a></p>
-        <p><span class="glyphicon glyphicon-ok" style="color:green;"></span> Gratis afhaling</p>
-        <p><span class="glyphicon glyphicon-ok" style="color:green;"></span> Gratis levering</p>
-        <p><span class="glyphicon glyphicon-ok" style="color:green;"></span> 30 dagen bedenktijd</p>
+        <p><span class='glyphicon glyphicon-ok' style='color:green;'></span> Gratis afhaling</p>
+        <p><span class='glyphicon glyphicon-ok' style='color:green;'></span> Gratis levering</p>
+        <p><span class='glyphicon glyphicon-ok'style='color:green;'></span> 30 dagen bedenktijd</p>
     </div>
-</div>';
+</div>";
         if (count($this->model->sameCategory) > 0) {
-            echo '<h3>Here\'s a selection from the same category</h3>';
+            echo "<h3>Here's a selection from the same category</h3>";
 
             Bike::renderBikes($this->model->sameCategory);
         }
